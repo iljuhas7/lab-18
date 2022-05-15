80de48179ad95f69421859670fa5d778c74f659c
iljuhas7 <iljuhas95@mail.ru>
2022-05-16 02:40:15 +0300

    individual task 2

---
+++ individuals/invidual_2/inv_properties.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+schema = {
+    "type": "object",
+    "properties": {
+        "name" : { "type": "string" },
+        "number" : { "type": "number" },
+        "z" : { "type": "number" }
+    }
+}
